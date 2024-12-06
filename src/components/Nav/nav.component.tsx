794d920628f0e77702e32296e47b886ff05e07d9
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-12-06 16:23:15 +0530

    Update Nav component: change padding, font size, and background height

--- src/components/Nav/nav.component.tsx
+++ src/components/Nav/nav.component.tsx
@@ -1,7 +1,7 @@
 export default function Nav() {
     return (
-        <div className="flex justify-between items-center px-8 bg-green-300 h-9">
-            <div className="font-semibold">Todo</div>
+        <div className="flex justify-between items-center px-12 bg-green-300 h-14">
+            <div className="text-2xl font-semibold">Todo</div>
         </div>
     );
 }
