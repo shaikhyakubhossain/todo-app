d9c0bb74e8e9be0389277508730c895600611798
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-12-24 17:06:38 +0530

    Add Button component with customizable children and click handler

---
+++ src/components/Button/button.component.tsx
@@ -0,0 +1,14 @@
+type propsType = {
+    children: React.ReactNode;
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+export default function Button(props: propsType) {
+  return (
+    <button onClick={props.onClick} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
+      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        {props.children}
+      </span>
+    </button>
+  );
+}
